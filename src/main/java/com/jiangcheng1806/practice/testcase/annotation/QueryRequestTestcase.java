edea003ef59cce1641bd502bf506ba4a09bef970
jiangcheng <jiangcheng1806@163.com>
2022-06-12 14:20:16 +0800

    测试jdk自带的NotNull主街判空，获取不到该注解

---
+++ src/main/java/com/jiangcheng1806/practice/testcase/annotation/QueryRequestTestcase.java
@@ -0,0 +1,46 @@
+package com.jiangcheng1806.practice.testcase.annotation;
+
+import com.sun.istack.internal.NotNull;
+
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * @author ：jiangcheng
+ * @version : 1.0.0
+ * @description ：TODO
+ * @date ： 2022/6/11 10:00
+ */
+public class QueryRequestTestcase extends BaseRequest {
+
+    @NotNull
+    private String name;
+    private Boolean gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getGender() {
+        return gender;
+    }
+
+    public void setGender(Boolean gender) {
+        this.gender = gender;
+    }
+
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+
+        QueryRequestTestcase testcase = new QueryRequestTestcase();
+        testcase.setGender(Boolean.FALSE);
+//        testcase.setName("dd");
+
+        testcase.nullFieldValidate();
+
+    }
+
+
+}
